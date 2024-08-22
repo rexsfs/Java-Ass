ca2833a00587376e1e2b1a1a4663ed386f63187c
rexsfs <yjee0618@gmail.com>
2024-08-22 22:17:33 +0800

    oder and payment

---
+++ Payment.java
@@ -0,0 +1,5 @@
+public class Payment {
+    private String paymentId;
+    private Float totalAmount;
+    private String status;
+}
--- order.java
+++ order.java
@@ -1,3 +1,7 @@
-public class order {
-    
+public class Order {
+    private String orderId;
+    private Integer qty;
+    private String orderDate;
+    private Float amount;
+    private Payment payment;
 }
