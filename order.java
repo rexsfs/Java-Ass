9be7b96612cbcba3a122e06c90fd2f2321cec635
rexsfs <yjee0618@gmail.com>
2024-09-01 22:23:11 +0800

    tryr

--- order.java
+++ order.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Order {
     private String orderId;
     private Integer qty;
     private String orderDate;
     private Float amount;
     private Payment payment;
+
+    public static void main(String[] args) {
+        System.out.println(String.format("| %-20s | %-20s | %-20s | %-20s |", "Id", "Qty", "Total", "Date"));
+    }
+
+    public Order(String orderId, Integer qty, String orderDate, Float amount, Payment payment) {
+        this.orderId = orderId;
+        this.qty = qty;
+        this.orderDate = orderDate;
+        this.amount = amount;
+        this.payment = payment;
+    }
+
+    public void addOrder() {
+        
+    }
 }
